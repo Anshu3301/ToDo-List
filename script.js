dcfaf223190cc94f0b847fd063d8e9039040d9e6
Anshu <134282893+Anshu3301@users.noreply.github.com>
2024-04-03 00:51:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+/* Key Learnings:
+    1. DOM Manipulation (child.parentElement, parent.childElementCount, parent.children[1], parent.removeChild())
+    2. Transparent-Hazy card (CSS, line:44-49)
+    3. remove img Background (CSS, line:21-22)
+*/
+
+let outer = document.getElementById('outer');
+let button = document.getElementById('add');
+let check,names,edit,todelete,create;
+
+button.addEventListener('click',(e)=>{
+    if(input.value != ''){
+        (create = () =>{
+        let newtask = document.createElement('div')
+        newtask.innerHTML = `<div class='task'>
+        <span class="name">${input.value}</span>
+        <div id="edit_delete">
+           <div class="edit"><button class="edit"><i class="fa-solid fa-pen fa-xl "></i></button></div>
+           <div class="todelete"><button class="todelete"><i class="fa-solid fa-trash fa-xl"></i></button></div>
+        </div>
+        </div>` ; 
+        // if(outer.childElementCount<=10){
+        //     outer.style.height = `50vh`;
+        // }
+        // else{
+        //     outer.style.height = `auto`;
+        // }
+        outer.append(newtask);      
+        input.value = '';
+        })()                                //IIFE
+
+        check = Array.from(document.querySelectorAll('.task'));
+        names = document.querySelectorAll('.name');
+        edit = document.querySelectorAll('.edit');
+        todelete = Array.from(document.querySelectorAll('.todelete'));
+    
+
+        todelete.forEach((b)=>{
+            b.addEventListener('click', (e)=>{
+                //console.log(e.target.parentElement.parentElement);
+                e.target.parentElement.parentElement.parentElement.parentElement.parentElement.removeChild(e.target.parentElement.parentElement.parentElement.parentElement);
+                // if(outer.childElementCount<=10){
+                //     outer.style.height = `50vh`;
+                // }
+                // else{
+                //     outer.style.height = `auto`;
+                // }
+            })
+        })
+        check.forEach((b)=>{
+            b.addEventListener('dblclick', (e)=>{
+                    e.target.children[0].style.textDecoration = 'line-through';
+                    //console.log(e.target.children[1].children[0]);
+                    e.target.children[1].children[0].style.visibility = 'hidden';
+            })
+        })
+        edit.forEach((b)=>{
+            b.addEventListener('click', (e)=>{
+                // if(outer.childElementCount<=10){
+                //     outer.style.height = `50vh`;
+                // }
+                // else{
+                //     outer.style.height = `auto`;
+                // }
+                input.value = String(e.target.parentElement.parentElement.parentElement.parentElement.children[0].innerText);
+                //console.log(e.target.parentElement.parentElement.parentElement);
+                e.target.parentElement.parentElement.parentElement.parentElement.parentElement.removeChild(e.target.parentElement.parentElement.parentElement.parentElement);
+            })
+        })
+   
+    }
+    else{
+        alert("Enter a Task!");
+    }
+})
+
+
+
+
+
